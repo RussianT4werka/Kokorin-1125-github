d11e486e3c3e49545092c07b73b9ac2663663e99
sanya <sanya@DESKTOP-2G1J4NA>
2021-10-25 12:37:05 +1000

    Добавьте файлы проекта.

---
+++ Кокорин 1125 github/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Кокорин_1125_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int b = rand.Next(100);
+            int count = 1;
+            Console.WriteLine("Компьютер загадал число от 0 до 100. Попробуйте отгадать его за три попытки.");
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            while (count <= 1000)
+            {
+                if (b == a)
+                {
+                    Console.WriteLine("Да! Компьютер загадал число " + a + "!");
+                    break;
+                }
+                { 
+                    if (b > a) Console.WriteLine("Больше!");
+                    if (b < a) Console.WriteLine("Меньше!");
+                    a = Convert.ToInt32(Console.ReadLine());  
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
